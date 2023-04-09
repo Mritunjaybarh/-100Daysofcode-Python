7f2faa13ccc55a72150e7cafc98314b570e1194b
Mritunjaybarh <113783380+Mritunjaybarh@users.noreply.github.com>
2023-04-10 00:30:43 +0530

    Add files via upload

---
+++ 1st day.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+1+2
+
+
+# In[2]:
+
+
+1*7
+
+
+# In[3]:
+
+
+2+5
+
+
+# In[4]:
+
+
+3+3/3
+
+
+# In[5]:
+
+
+2+52*254855*4557125*22-+541155+1174
+
+
+# In[6]:
+
+
+2/8
+
+
+# In[12]:
+
+
+type("Mritunjay")
+
+
+# In[13]:
+
+
+type(7.8)
+
+
+# In[15]:
+
+
+type("Mk")
+
+
+# In[17]:
+
+
+type('MY name is Mritunjay')
+
+
+# In[18]:
+
+
+type("a")
+
+
+# In[24]:
+
+
+type("ram is a boy")
+
+
+# In[29]:
+
+
+10*"str"
+
+
+# In[27]:
+
+
+type()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
