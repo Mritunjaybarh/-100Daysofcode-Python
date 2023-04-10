9efd55efb81d52f11275cac384c69413acbe436e
Mritunjaybarh <113783380+Mritunjaybarh@users.noreply.github.com>
2023-04-11 00:53:08 +0530

    Add files via upload

---
+++ 2nd days.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+first_name = "mritunjay" 
+last_name = "kumar"
+
+
+# In[3]:
+
+
+print("My first_name is  () and last_name is ( )  ".format())
+
+
+# In[8]:
+
+
+input(print ("My name is Mritunajy kumar"))
+
+
+# In[4]:
+
+
+num=7
+amt=345.56
+code="a"
+pi=3.14153
+pop_of_india=1225454555
+msg="Hi"
+print("Num ="+str(num))
+print("amount ="+str(amt))
+print("population of india is ="+str(pop_of_india))
+
+
+# In[6]:
+
+
+val="hello"
+print(val)
+val=180
+print(val)
+val=12.34
+print(val)
+
+
+# In[8]:
+
+
+x=5
+y=10
+print("hello")
+print(x+y)
+
+
+# In[ ]:
+
+
+
+
