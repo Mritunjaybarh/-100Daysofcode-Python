5e3588cd789d2ff353dd5d3b5893ca42aa200736
Mritunjaybarh <113783380+Mritunjaybarh@users.noreply.github.com>
2023-04-14 01:25:43 +0530

    Add files via upload

---
+++ 4thdays .py
@@ -0,0 +1,594 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+l=['ram','shyam','mohan','sohan']
+
+
+# In[9]:
+
+
+l1=[]
+for i in l:
+    print(i)
+l1.append(i.upper())
+
+
+# In[14]:
+
+
+l1
+
+
+# In[15]:
+
+
+l=[1,2,3,4,"sudh","kumar",324,34.56,"abc"]
+
+
+# In[1]:
+
+
+l1_NUM[]
+l2_string[]
+for i in l:
+    if type(i)== int or type(i)==float:
+        l1_num.apend(i)
+    else :
+        12_str.append(i)
+    
+    
+
+
+# In[17]:
+
+
+l
+
+
+# In[18]:
+
+
+l=[1,2,3,4,5,6]
+
+
+# In[25]:
+
+
+for i in l:
+    print(i,type(i))
+
+
+# In[26]:
+
+
+l1 =['ram','shyam','mohan','rohan']
+
+
+# In[31]:
+
+
+for i in l1:
+    print(i)
+else :
+     print("if for loop is able to complete itself then only else will execute")
+
+
+# In[32]:
+
+
+l1
+
+
+# In[35]:
+
+
+for i in l1 :
+    if 1 =="kumar":
+        break
+        print(i)
+else :
+    print("execute this if for lop is able to complete itself ")
+
+
+# In[1]:
+
+
+
+
+
+# In[2]:
+
+
+print("HEllO python world")
+
+
+# In[2]:
+
+
+message = "Hello python"
+print(message)
+
+
+# In[3]:
+
+
+text="Ram is a good boy"
+print(text)
+text="ram is a bad boy"
+print(text)
+
+
+# In[11]:
+
+
+s="i am a  good boy///"
+print(s)
+
+
+# In[15]:
+
+
+name="Mritunjay kumar"
+print(name.title())
+
+
+# In[16]:
+
+
+print(name.upper())
+
+
+# In[17]:
+
+
+print(name.lower())
+
+
+# In[26]:
+
+
+first="Mritunjay"
+last="kumar"
+full_name=f'{first} {last}'
+print(full_name)
+
+
+# In[47]:
+
+
+full_name1=f'Hello,{full_name.upper()}'
+print(full_name1)
+
+
+# In[48]:
+
+
+mess= "my fsther's name is "
+print(mess)
+
+
+# In[51]:
+
+
+mess= 'My father's name is '
+print(mess)
+
+
+# In[57]:
+
+
+famous_quotes="a person who never made a mistake never tried anything new "
+famous_quotes1= f'Albert einstien once said  {famous_quotes}'
+print(famous_quotes1)
+
+
+# In[63]:
+
+
+famous_person ="albert einstien"
+message=f'{famous_person } { famous_quotes}'
+print(message)
+
+
+# In[64]:
+
+
+0.2+0.1
+
+
+# In[65]:
+
+
+4/2
+
+
+# In[66]:
+
+
+2*2.0
+
+
+# In[67]:
+
+
+5+3
+
+
+# In[68]:
+
+
+4*2
+
+
+# In[69]:
+
+
+9-1
+
+
+# In[70]:
+
+
+7+2-5*5
+
+
+# In[74]:
+
+
+favorite_number = 15
+    print(favorite_number)
+
+
+# In[75]:
+
+
+Now is better than never
+
+
+# In[76]:
+
+
+course = ["bca","mca",'Mtech']
+print(course)
+
+
+# In[77]:
+
+
+print(course[0])
+
+
+# In[78]:
+
+
+print(course[-1])
+
+
+# In[82]:
+
+
+mess =f"My first course is {course[0].title()}"
+print(mess)
+
+
+# In[103]:
+
+
+name =["chotu","pawan","rishu","bittu"]
+
+
+# In[119]:
+
+
+print(name)
+
+
+# In[120]:
+
+
+print(name[0])
+
+
+# In[121]:
+
+
+print(name[1])
+
+
+# In[122]:
+
+
+message =f'Have a nice day {name[0]}'
+print(message)
+
+
+# In[123]:
+
+
+message = f'Have a good job {name[1]}'
+print(message)
+
+
+# In[125]:
+
+
+message =f'have a full joy {name[-1]}'
+print(message)
+
+
+# In[126]:
+
+
+fvrt_transport= ['car','bike','scooty','truck']
+
+
+# In[127]:
+
+
+print(fvrt_transport)
+
+
+# In[131]:
+
+
+statement=f' "I would like to own a honda {fvrt_transport[1]}"'
+print(statement)
+
+
+# In[132]:
+
+
+bike=['suzuki','ducati','yamaha']
+
+
+# In[137]:
+
+
+bike[0]='honda'
+print(bike)
+
+
+# In[142]:
+
+
+bike.append('suzuki')
+print(bike)
+
+
+# In[141]:
+
+
+print(bike)
+
+
+# In[147]:
+
+
+guest_name =['chotu','pawan','manish']
+
+
+# In[148]:
+
+
+print(f'you are invited for dinner {guest_name[0]}')
+
+
+# In[149]:
+
+
+print(f'you are invited for dinner {guest_name[1]}')
+
+
+# In[150]:
+
+
+print(f'you are invited for dinner {guest_name[2]}')
+
+
+# In[151]:
+
+
+guest_name[0]='mohan'
+
+
+# In[152]:
+
+
+print(guest_name)
+
+
+# In[153]:
+
+
+print(f'you are invited for dinner {guest_name[0]}')
+
+
+# In[154]:
+
+
+print(f'you are invited for dinner {guest_name[1]}')
+
+
+# In[155]:
+
+
+print(f'you are invited for dinner {guest_name[2]}')
+
+
+# In[159]:
+
+
+guest_name.insert( 0,"ram")
+
+
+# In[160]:
+
+
+print(guest_name)
+
+
+# In[163]:
+
+
+guest_name.insert( 3,"ravi")
+
+
+# In[164]:
+
+
+guest_name.insert( 5,"pintu")
+print(guest_name)
+
+
+# In[168]:
+
+
+guest_name.insert(0,'ram')
+print(guest_name)
+
+
+# In[169]:
+
+
+guest_name.append("sonu")
+print(guest_name)
+
+
+# In[178]:
+
+
+print(f"you are invited for dinner {guest_name[0]}")
+
+
+# In[179]:
+
+
+print(f"you are invited for dinner {guest_name[1]}")
+
+
+# In[180]:
+
+
+print(f"you are invited for dinner {guest_name[2]}")
+
+
+# In[181]:
+
+
+print(f"you are invited for dinner {guest_name[3]}")
+
+
+# In[182]:
+
+
+print(f"you are invited for dinner {guest_name[4]}")
+
+
+# In[183]:
+
+
+print(f"you are invited for dinner {guest_name[5]}")
+
+
+# In[184]:
+
+
+cars=["bmw","audi","toyato","range rover"]
+
+
+# In[186]:
+
+
+cars.sort()
+print(cars)
+
+
+# In[189]:
+
+
+cars.sort(reverse=1)
+print(cars)
+
+
+# In[191]:
+
+
+
+print(cars)
+cars.reverse()
+print(cars)
+
+
+# In[195]:
+
+
+print(cars)
+for car in cars :
+    print(cars)
+
+
+# In[8]:
+
+
+l=[1,2,3,4,5]
+l1=[]
+for i in l:
+    l1.append(i**2)
+
+
+# In[9]:
+
+
+l1
+
+
+# In[10]:
+
+
+l
+
+
+# In[11]:
+
+
+[i for i in l if i% 2 ==0]
+
+
+# In[12]:
+
+
+l1=["ram","shyam","mohan","data science"]
+
+
+# In[13]:
+
+
+[i.upper()for i in l1]
+
+
+# In[14]:
+
+
+l
+
+
+# In[16]:
+
+
+list(i**2 for i in l)
+
+
+# In[19]:
+
+
+d ={"key1 ":1,"key2":2}
+
+
+# In[ ]:
+
+
+
+
